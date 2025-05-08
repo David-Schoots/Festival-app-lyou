d9014133616f8ae50b90ef630240efaaacecd619
David-Schoots <davidschoots@live.nl>
2025-05-08 13:46:07 +0100

    first commit

---
+++ festival-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justiy-center items-center h-screen">
+      <img src="/icon-512x512.png" alt="" /> {/* Logo */}
+    </div>
+  );
+}
+
+export default App;
---
+++ festival-app/vite.config.js
@@ -0,0 +1,41 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(), // React plugin voor Vite
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate", // automatische update van de service worker
+      devOptions: {
+        enabled: true, // Zorg ervoor dat de PWA ook in de development omgeving geactiveerd wordt
+      },
+      manifest: {
+        name: "❤️U Festival",
+        short_name: "❤️U",
+        description: "We ❤️ Festival",
+        theme_color: "#247BA0",
+        display: "standalone",
+        orientation: "portrait",
+        start_url: "/",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
