c147bb5498af6305d1b3ec1d54d69e5c783c295f
David-Schoots <davidschoots@live.nl>
2025-05-09 09:48:42 +0100

    added begin to frontend for the carousel cards

--- festival-app/src/Home.jsx
+++ festival-app/src/Home.jsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { CiSearch } from "react-icons/ci";
 
 function Home() {
   return (
@@ -11,7 +12,36 @@ function Home() {
         duration: 1,
       }}
     >
-     {/* searchbar */}
+      {/* Search bar for searching artist */}
+      <div className="flex items-center justify-center mt-4 mb-6">
+        <div className="relative w-8/12">
+          <span className="absolute left-4 top-1/2 transform -translate-y-1/2">
+            <CiSearch className="text-gray-500" />
+          </span>
+          <input
+            type="text"
+            placeholder="Search an artist..."
+            className="p-4 pl-12 border border-gray-300 rounded-4xl shadow-md w-full"
+          />
+        </div>
+      </div>
+
+      <h2 className="text-4xl text-[#247BA0] font-bold mt-12 ml-8 mb-8">
+        Festival Stages
+      </h2>
+      <div className="flex justify-center">
+        <div className="w-[50%] rounded overflow-hidden shadow-lg">
+          <img src="https://placehold.co/200x200" className="w-full" alt="" />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Voluptatibus quia, nulla! Maiores et perferendis eaque,
+              exercitationem praesentium nihil.
+            </p>
+          </div>
+        </div>
+      </div>
     </motion.div>
   );
 }
