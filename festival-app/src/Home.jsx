31e16e48c98bdd313a1cb58c604d71293956931f
David-Schoots <davidschoots@live.nl>
2025-05-09 09:52:50 +0100

    added new design to cards

--- festival-app/src/Home.jsx
+++ festival-app/src/Home.jsx
@@ -1,5 +1,7 @@
 import { motion } from "framer-motion";
 import { CiSearch } from "react-icons/ci";
+import { FiInfo } from "react-icons/fi";
+import { GoLocation } from "react-icons/go";
 
 function Home() {
   return (
@@ -29,16 +31,25 @@ function Home() {
       <h2 className="text-4xl text-[#247BA0] font-bold mt-12 ml-8 mb-8">
         Festival Stages
       </h2>
+
       <div className="flex justify-center">
-        <div className="w-[50%] rounded overflow-hidden shadow-lg">
-          <img src="https://placehold.co/200x200" className="w-full" alt="" />
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-            <p className="text-gray-700 text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Voluptatibus quia, nulla! Maiores et perferendis eaque,
-              exercitationem praesentium nihil.
-            </p>
+        <div className="relative w-60 h-96 rounded-3xl overflow-hidden shadow-lg">
+          {/* Achtergrondafbeelding */}
+          <img
+            src="https://placehold.co/400x500"
+            alt="Mountains"
+            className="w-full h-full object-cover"
+          />
+
+          {/* Info icoon */}
+          <div className="absolute top-3 right-3  rounded-full p-1">
+            <FiInfo className="text-gray-800 text-xl" />
+          </div>
+
+          {/*  label onderaan */}
+          <div className="absolute bottom-4 left-4 right-4 bg-[#0A2A43] text-white rounded-xl flex items-center px-4 py-2">
+            <GoLocation className="mr-2" />
+            <span>Temp Artist</span>
           </div>
         </div>
       </div>
