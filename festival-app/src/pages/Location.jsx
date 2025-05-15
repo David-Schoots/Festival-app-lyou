ed69b0d1af2b658eb164d8beddc80db8a398db7b
David-Schoots <davidschoots@live.nl>
2025-05-15 09:12:59 +0200

    bug fix in location.jsx

--- festival-app/src/pages/Location.jsx
+++ festival-app/src/pages/Location.jsx
@@ -14,7 +14,7 @@ export default function Location() {
       <h2 className="text-4xl text-[#247BA0] font-bold mt-12 mb-8">Location</h2>
       <div className="w-full max-w-xs h-[600px] rounded-lg overflow-hidden shadow-lg mb-8">
         <MapContainer
-          crs={L.CRS.Simple}
+          crs={L.CRS.Simple}  
           bounds={bounds}
           maxBounds={bounds}
           maxBoundsViscosity={1.0}
@@ -23,8 +23,17 @@ export default function Location() {
         >
           <ImageOverlay url={mapImage} bounds={bounds} />
           {/* Example marker: */}
-          <Marker position={[500, 500]}>
-            <Popup>Festival Main Stage</Popup>
+          <Marker position={[1850, 480]}>
+            <Popup>Ponton</Popup>
+          </Marker>
+          <Marker position={[1060, 710]}>
+            <Popup>The Lake</Popup>
+          </Marker>
+          <Marker position={[700, 800]}>
+            <Popup>The Club</Popup>
+          </Marker>
+          <Marker position={[200, 1100]}>
+            <Popup>The Hanggar</Popup>
           </Marker>
         </MapContainer>
       </div>
