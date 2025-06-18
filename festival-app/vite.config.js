910fda8d43acde1515c3bf9787a40f314f39952e
David-Schoots <davidschoots@live.nl>
2025-06-18 14:38:42 +0200

    added fix with composer and added animation to hero

--- festival-app/vite.config.js
+++ festival-app/vite.config.js
@@ -6,8 +6,8 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/web/dist/",
-  plugins: [
+  /*   base: "/web/dist/",
+   */ plugins: [
     react(),
     tailwindcss(),
     VitePWA({
